d3c58a785794c598e36532bdf7f935cc7c6fd134
Aso2001012 <83050448+Aso2001012@users.noreply.github.com>
2021-07-30 23:34:54 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class Main{
   public static void main(String[] arg){
       Scanner scanner=new Scanner(System.in);
+    System.out.println("********************");
     System.out.println("【戦艦ゲーム】");
+   
     System.out.println("爆弾のX座標を入力してください");
     int x=scanner.nextInt();
     System.out.println(x);
